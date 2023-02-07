60e0f6ea22e9ba3d2f45f71ab8dca368ba319b98
LihDev <liihdevfranca@gmail.com>
2023-02-07 11:46:55 -0300

    images and receitas

---
+++ src/receitas/index.js
@@ -0,0 +1,16 @@
+const [items] = useState([
+    {
+        id: 1,
+        tituloReceita: 'Cebola enrolada no bacon',
+        ingredientes: 
+        [
+            '3 Cebolas Grandes',
+            '3 Limões',
+            '500g de bacon em tiras',
+            'Pimenta a gosto',
+            'Sal',
+            'Palitos de madeira',
+        ],
+
+    }
+])
